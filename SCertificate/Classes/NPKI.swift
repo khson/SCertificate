00c68712d14824328d84c18a0758ebae5a80892b
khson <email.khson@gmail.com>
2019-08-06 19:30:26 +0900

    [FEATURE] Add X590, ASN1

--- SCertificate/Classes/NPKI.swift
+++ SCertificate/Classes/NPKI.swift
@@ -7,15 +7,50 @@
 
 import Foundation
 
-extension Certificate.NPKI {
-    public func cleanUp() {
-        let fileManager = FileManager.default
+public struct NPKI: Certificate {
+    static let Root = "NPKI"
+    static let certFile = "signCert.der"
+    static let keyFile = "signPri.key"
+    static let errorCertFile = "SignCert.der"
+    static let errorKeyFile = "SignPri.key"
+    let fileManager = FileManager.default
+    let institutions = [
+        "CrossCert/USER",
+        "CrossCert/User",
+        "KICA/USER",
+        "KICA/User",
+        "KISA/USER",
+        "KISA/User",
+        "NCASign/USER",
+        "NCASign/User",
+        "SignKorea/USER",
+        "SignKorea/User",
+        "TradeSign/USER",
+        "TradeSign/User",
+        "yessign/USER",
+        "yessign/User"
+    ]
+    
+    public init() {
+        cleanUp()
+    }
+}
+
+extension NPKI {
+    private func getRootPath() -> URL{
         // swiftlint:disable:next force_try
         let documentDir = try! fileManager.url(for: .documentDirectory,
                                                in: .userDomainMask,
                                                appropriateFor: nil,
                                                create: true)
-        let rootPath = documentDir.appendingPathComponent(Certificate.NPKI.Root, isDirectory: true)
+        return documentDir.appendingPathComponent(NPKI.Root, isDirectory: true)
+    }
+}
+
+extension NPKI {
+    public func cleanUp() {
+        let rootPath = getRootPath()
+        
         institutions.forEach { cert in
             let certPath = rootPath.appendingPathComponent(cert, isDirectory: true)
             if fileManager.fileExists(atPath: certPath.path) {
@@ -24,12 +59,12 @@ extension Certificate.NPKI {
                     dirList.forEach { dir in
                         let finalPath = certPath.appendingPathComponent(dir, isDirectory: true)
                         
-                        let signDer = finalPath.appendingPathComponent(Certificate.NPKI.errorCertFile)
-                        let signKey = finalPath.appendingPathComponent(Certificate.NPKI.errorKeyFile)
+                        let signDer = finalPath.appendingPathComponent(NPKI.errorCertFile)
+                        let signKey = finalPath.appendingPathComponent(NPKI.errorKeyFile)
                         if fileManager.fileExists(atPath: signDer.path) &&
                             fileManager.fileExists(atPath: signKey.path) {
-                            let orginSignDer = finalPath.appendingPathComponent(Certificate.NPKI.certFile)
-                            let orginSignKey = finalPath.appendingPathComponent(Certificate.NPKI.keyFile)
+                            let orginSignDer = finalPath.appendingPathComponent(NPKI.certFile)
+                            let orginSignKey = finalPath.appendingPathComponent(NPKI.keyFile)
                             do {
                                 _ = try fileManager.moveItem(at: signDer, to: orginSignDer)
                                 _ = try fileManager.moveItem(at: signKey, to: orginSignKey)
@@ -45,17 +80,13 @@ extension Certificate.NPKI {
             }
         }
     }
-    
+}
+
+extension NPKI {
     public func getList() -> [String] {
         var npkiList = [String]()
+        let rootPath = getRootPath()
         
-        let fileManager = FileManager.default
-        // swiftlint:disable:next force_try
-        let documentDir = try! fileManager.url(for: .documentDirectory,
-                                               in: .userDomainMask,
-                                               appropriateFor: nil,
-                                               create: true)
-        let rootPath = documentDir.appendingPathComponent(Certificate.NPKI.Root, isDirectory: true)
         institutions.forEach { cert in
             let certPath = rootPath.appendingPathComponent(cert, isDirectory: true)
             if fileManager.fileExists(atPath: certPath.path) {
@@ -63,12 +94,12 @@ extension Certificate.NPKI {
                     let dirList = try fileManager.contentsOfDirectory(atPath: certPath.path)
                     dirList.forEach { dir in
                         let finalPath = certPath.appendingPathComponent(dir, isDirectory: true)
-                        let signDer = finalPath.appendingPathComponent(Certificate.NPKI.certFile)
-                        let signKey = finalPath.appendingPathComponent(Certificate.NPKI.keyFile)
+                        let signDer = finalPath.appendingPathComponent(NPKI.certFile)
+                        let signKey = finalPath.appendingPathComponent(NPKI.keyFile)
                         
                         if fileManager.fileExists(atPath: signDer.path) &&
                             fileManager.fileExists(atPath: signKey.path) {
-                            npkiList.append(String(Certificate.NPKI.Root + "/\(cert)/\(dir)"))
+                            npkiList.append(String(NPKI.Root + "/\(cert)/\(dir)"))
                         }
                     }
                 } catch {
@@ -76,7 +107,6 @@ extension Certificate.NPKI {
                 }
             }
         }
-        
         return npkiList
     }
 }
